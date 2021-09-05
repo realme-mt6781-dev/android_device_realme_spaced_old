adc4e6b97b04fe57422054ad4cbf633c00bf6a57
novenary <streetwalkermc@gmail.com>
2021-09-05 18:58:52 +0300

    spaced: sensors: Implement ALS compensation algorithm
    Based on thorough analysis of OnePlus' libsensorservice
    
    This is a work-in-progress
    
    Change-Id: I82f922309d607e28cd0d4b4fb5fe38fa32d12242

--- device.mk
+++ device.mk
@@ -427,6 +427,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service-multihal.spaced \
+    als_correction_service.spaced \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
