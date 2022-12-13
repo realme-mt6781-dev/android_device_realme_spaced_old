7c87a4ad782b2756b44bdae68672797413877547
bengris32 <bengris32@protonmail.ch>
2022-12-13 19:48:41 +0000

    spaced: Build required media libs * Mainly dependencies of C2 HAL.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Icc8ecc63094a4d1fb9d669f9da93184b285fd471

--- device.mk
+++ device.mk
@@ -108,6 +108,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.spaced
 
+# Media
+PRODUCT_PACKAGES += \
+    libcodec2_hidl@1.1.vendor \
+    libcodec2_hidl@1.2.vendor \
+    libavservices_minijail_vendor \
+    libstagefright_softomx_plugin.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libcodec2_soft_common.vendor
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6781 \
