8525c19b5fd21a4004664304abc48c323476f1d3
bengris32 <bengris32@protonmail.ch>
2022-12-13 19:51:52 +0000

    spaced: Build default Renderscript impl
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I93739b8d0843b0def8f95cf2ab64abe11f20e581
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio@1.6.vendor
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6781 \
