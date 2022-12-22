2dfe2968e013220918419eb4d0673d60d2011b03
Arne Coucheron <arco68@gmail.com>
2022-12-22 11:04:35 +0000

    spaced: Disable serial console notification Google added a check for this prop in R, and if it's running it will show a notification about it that it's impacting performance. Why it's not disabled by default I don't know, so just force disable it here.
    [bengris32] Convert to standalone init script and install to system_ext
    
    Change-Id: I37dfcfce504ef248ce6517b95a0f5c889e580dd9
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,6 +2,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.ago.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
