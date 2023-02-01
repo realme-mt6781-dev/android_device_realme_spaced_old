19b97d30002b64f24731fd1a8b6cd838fbc83ced
bengris32 <bengris32@protonmail.ch>
2023-02-01 17:55:04 +0000

    spaced: Switch to common Vibrator AIDL
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I6c446e8c05c9ea970090ce6ae3aa041cc2fe633a

--- device.mk
+++ device.mk
@@ -416,6 +416,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@1.0-impl
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.mediatek
+
 # VNDK
 PRODUCT_PACKAGES += \
     libutils-v32 \
