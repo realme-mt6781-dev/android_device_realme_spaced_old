5f424e0e07ea0be3418bcda6e6ea35380bc9ff9b
bengris32 <bengris32@protonmail.ch>
2023-02-05 20:53:17 +0000

    spaced: Build Oplus Touch HAL and address denials
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ibdd13e6989adc9b11064d7c99345bf38b6b863b5
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -423,6 +423,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@1.0-impl
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.oplus
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
