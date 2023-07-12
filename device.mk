886a74f13ace2be00aec7e055a15458a50bf3f74
HELLINFIX <mridulsharma57570@gmail.com>
2023-07-12 12:10:27 +0000

    spaced: drop livedisplay

--- device.mk
+++ device.mk
@@ -208,10 +208,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.spaced
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service-spaced
-
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.1.vendor \
