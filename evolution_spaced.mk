3b26536efc644016cdca2748fb29dcc2c70a1a62
dinonomous <133356394+dinonomous@users.noreply.github.com>
2023-07-14 14:56:27 +0530

    Update evolution_spaced.mk
    # Inherit some common EvolutionX stuff
    $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
    
    # EvolutionX Specific Flags
    EVO_BUILD_TYPE := UNOFFICIAL
    EXTRA_UDFPS_ANIMATIONS := false
    TARGET_BOOT_ANIMATION_RES := 1080
    TARGET_SUPPORTS_QUICK_TAP := true
    TARGET_USES_PICO_GAPPS := true
    TARGET_BUILD_APERTURE_CAMERA := false
    
    # Device identifier
    PRODUCT_NAME := evolution_spaced
    
    Signed-off-by: dinonomous <133356394+dinonomous@users.noreply.github.com>

--- evolution_spaced.mk
+++ evolution_spaced.mk
@@ -11,15 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/realme/spaced/device.mk)
 
-# Inherit some common AospOS stuff.
+# Inherit some common EvolutionX stuff
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_spaced
+# EvolutionX Specific Flags
+EVO_BUILD_TYPE := UNOFFICIAL
+EXTRA_UDFPS_ANIMATIONS := false
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_PICO_GAPPS := true
+TARGET_BUILD_APERTURE_CAMERA := false
+
+# Device identifier
+PRODUCT_NAME := evolution_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 8i
-
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 # Build info
